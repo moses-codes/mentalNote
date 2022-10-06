806bf32ad56564c4da241f77b415bdc49bbaafc8
moses-codes <moses.ilagan@gmail.com>
2022-10-06 14:51:29 -0700

    added edit story functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
+const methodOverride = require('method-override')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
@@ -18,15 +19,41 @@ connectDB()
 
 const app = express()
 
+//BODY PARSER
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+  )
+
 //Logging
 if(process.env.NODE_ENV === 'dev'){
     app.use(morgan('dev'))
     //makes sure that morgan runs in DEV mode only
 }
 
+//Hamburger Helpers
+const {formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
+
 //Handlebars
 app.engine('.hbs', exphbs.engine(
     {   
+        helpers: {
+            formatDate,
+            stripTags,
+            truncate,
+            editIcon,
+            select,
+        },
         defaultLayout: 'main',
         extname: '.hbs'
     }
@@ -48,12 +75,19 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+//set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
+
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 
 const PORT = process.env.PORT || 5000
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,16 @@ const Story = require('../models/Story')
 //@desc Login/Landing page
 //@route GET /
 
-router.get('/', ensureGuest, async (req, res) => {
+router.get('/', ensureGuest, (req,res) => {
+    res.render('login',
+    {layout: 'login' }
+    )
+})
+
+//@desc Dashboard
+//@route GET /dashboard
+
+router.get('/dashboard', ensureAuth, async (req, res) => {
     try{
         const stories = await Story.find({ user: req.user.id}).lean()
         res.render('dashboard', {
@@ -20,16 +29,6 @@ router.get('/', ensureGuest, async (req, res) => {
         res.render('error/500')
 
     }
-})
-
-//@desc Dashboard
-//@route GET /dashboard
-
-router.get('/dashboard', ensureAuth, (req, res) => {
-    console.log(req.user)
-    res.render('dashboard', {
-        name: req.user.firstName
-    })
 })
 
 module.exports = router
